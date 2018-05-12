647ed7ae82064018b0d2934ed05b84a9bb78825c
Nicholas Powers <njpowers7915@gmail.com>
2018-05-12 04:48:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,10 @@
 class Dog
   attr_accessor :mood
   attr_reader :name
- @@dog_count = 0
-  
-  def self.count
-    @@dog_count
-  end
-  
- # def save
-#    @@dog_count << self
- # end
-  
+
   
   def initialize(name, mood = "nervous")
     @name = name
     @mood = mood
-    @@dog_count += 1
   end
 end
