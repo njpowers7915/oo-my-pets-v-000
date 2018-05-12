0492204d4a4f8300940b913c2683daef1f3ec79f
Nicholas Powers <njpowers7915@gmail.com>
2018-05-12 04:31:08 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,6 +1,16 @@
 class Cat
   attr_accessor :mood
   attr_reader :name
+ @@cat_count = []
+  
+  def count
+    @@dog_count
+  end
+  
+  def save
+    @@dog_count << self
+  end
+ 
   
   def initialize(name, mood = "nervous")
     @name = name
