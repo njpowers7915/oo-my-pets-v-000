d20bb2b189954e37b6658bbdb1b73fc3685dbe1a
Nicholas Powers <njpowers7915@gmail.com>
2018-05-12 04:49:00 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,20 +1,10 @@
 class Fish
   attr_accessor :mood
   attr_reader :name
-  @@fish_count = 0
-  
-  def self.count
-    @@fish_count
-  end
-  
- # def save
-  #  @@fish_count << self
-  #end
-  
+
   
   def initialize(name, mood = "nervous")
     @name = name
     @mood = mood
-    @@fish_count += 1
   end
 end
