0af80a07fe5ed12ab5c9f65edb6c9ea532866191
Nicholas Powers <njpowers7915@gmail.com>
2018-05-12 04:49:50 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -24,20 +24,14 @@ class Owner
   end
 
   def buy_fish(name)
-    new_fish = Fish.new(name)
-   # new_fish.save
-    pets[:fishes] << new_fish
+    pets[:fishes] << Fish.new(name)
   end
   
   def buy_cat(name)
-    new_cat = Cat.new(name)
-   # new_cat.save
     pets[:cats] << Cat.new(name)
   end
   
  def buy_dog(name)
-    new_dog = Dog.new(name)
-   # new_dog.save
     pets[:dogs] << Dog.new(name)
   end 
   
